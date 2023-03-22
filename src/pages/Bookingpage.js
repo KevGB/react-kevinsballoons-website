b1623828b85c0b6aaae7e31e2dda863b5d7f6788
KevGB <kevingbice@gmail.com>
2023-03-22 16:56:47 -0700

    Creates Bookingpage

--- src/pages/Bookingpage.js
+++ src/pages/Bookingpage.js
@@ -1,5 +1,17 @@
+import Header from "../components/Header";
+import BookingForm from "../components/booking-Form/bookingForm";
+
 const Bookingpage = () => {
-    return <div>Booking Page</div>;
-  };
-  
-  export default Bookingpage;
+  return (
+    <>
+      <Header
+        headline="Book Kevin"
+        tagline="World-famouse balloon twisting"
+        display="none"
+      />
+      <BookingForm />
+    </>
+  );
+};
+
+export default Bookingpage;
