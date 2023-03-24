0c5cce166a51bf01c51349bb2c29da7315825eb2
KevGB <kevingbice@gmail.com>
2023-03-24 07:17:01 -0700

    Works on Gallerypage

--- src/pages/Gallerypage.js
+++ src/pages/Gallerypage.js
@@ -3,12 +3,10 @@ import ScrollingGallery from "../components/ScrollingGallery";
 const Gallerypage = () => {
   return (
     <div>
-      <h1 className="start-headline">Lookit all the shit I can make!</h1>
+      <h1 className="form-headline">Lookit all the shit I can make!</h1>
       <ScrollingGallery />
       <ScrollingGallery />
-      <ScrollingGallery />
-      <ScrollingGallery />
-      <ScrollingGallery />
+      
      
     </div>
   );
