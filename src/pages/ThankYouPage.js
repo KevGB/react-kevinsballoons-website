2c342833d903da203733f03d7ea57ac02cd0e900
KevGB <kevingbice@gmail.com>
2023-04-05 18:01:37 -0700

    debugging

--- src/pages/ThankYouPage.js
+++ src/pages/ThankYouPage.js
@@ -1,5 +1,5 @@
 import Header from "../components/Header";
-import thankyou from "../app/assets/img/Thankyou.jpg";
+import thankyou from "../app/assets/img/thankyou.jpg";
 import "./pagescss/thankyou.css";
 
 const ThankYouPage = () => {
